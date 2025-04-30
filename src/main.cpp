aa35ba7d1b9a8884ba0f50dd5d404449d39afd7a
GoingJack <goingjack@foxmail.com>
2025-04-30 11:28:20 +0800

    feat: implement single instance application using shared memory

--- src/main.cpp
+++ src/main.cpp
@@ -3,6 +3,11 @@
 #include "mainwindow.h"
 
 int main(int argc, char *argv[]) {
+  QSharedMemory sharedMemory("QtMindMap");
+  if (sharedMemory.attach()) {
+    return 0;
+  }
+  sharedMemory.create(1);
   QApplication a(argc, argv);
   MainWindow w;
   w.show();
--- src/pch.h
+++ src/pch.h
@@ -32,6 +32,7 @@
 #include <QtMath>
 #include <QProcess>
 #include <QCoreApplication>
+#include <QSharedMemory>
 
 // Qt Internationalization
 #include <QTranslator>
